6de04e5249d8321047508a65d79b0fd19db498a7
ayakanishimura <a.tktk08@gmail.com>
2020-12-08 08:15:10 +0000

    task6.Finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,17 +5,12 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_params)
     @book_comment.book_id = @book.id
     @showbook = Book.new
-    if @book_comment.save
-    redirect_to book_path(@book.id)
-    else
-
-      render "books/show"
-    end
+    @book_comment.save
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
   end
 
    private
